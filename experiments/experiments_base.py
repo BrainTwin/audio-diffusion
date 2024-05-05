6b64ee2762e5bc53fb6a6253b9809b8817412311
timohromadka <timo.hromadka@gmail.com>
2024-05-05 14:34:47 +0100

    Modify base experiments with correct parameters

--- experiments/experiments_base.py
+++ experiments/experiments_base.py
@@ -38,14 +38,15 @@ for vars in variable_combinations:
     --n_fft 1024 \\
     --output_dir {vars['output_dir']} \\
     --train_batch_size 32 \\
-    --eval_batch_size 8 \\
+    --eval_batch_size 16 \\
     --num_epochs 1000 \\
+    --max_training_num_steps 300000 \\
     --gradient_accumulation_steps 4 \\
     --learning_rate 1e-4 \\
     --lr_warmup_steps 500 \\
     --mixed_precision no \\
-    --save_model_epochs 2 \\
-    --save_images_epochs 2 \\
+    --save_model_steps 50000 \\
+    --save_images_epochs 25000 \\
     --num_train_steps 1000 \\
     --num_inference_steps 1000 \\
     --train_scheduler ddpm \\
