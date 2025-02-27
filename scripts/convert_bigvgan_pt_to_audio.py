eff2a133346d0c4be7a86cf9fb36f25748082ad8
timohromadka <timo.hromadka@gmail.com>
2025-02-27 14:03:35 +0000

    Deleted convert_bigvgan_pt_to_audio.py

--- scripts/convert_bigvgan_pt_to_audio.py
+++ scripts/convert_bigvgan_pt_to_audio.py
@@ -4,13 +4,14 @@ import argparse
 import soundfile as sf
 from tqdm import tqdm
 
-import bigvgan
-
-device = 'cuda'
-
-def convert_bigvgan_tensor_to_audio(mel_tensor, output_path, model):
-
-      
+def convert_bigvgan_tensor_to_audio(mel_tensor, output_path, bigvgan_model_name):
+    device = 'cuda'
+    import bigvgan
+    
+    model = bigvgan.BigVGAN.from_pretrained(f'nvidia/{bigvgan_model_name}', use_cuda_kernel=False)
+    model.remove_weight_norm()
+    model = model.eval().to(device)
+    
     mel_tensor = mel_tensor.to(device)
     
     with torch.inference_mode():
@@ -20,18 +21,11 @@ def convert_bigvgan_tensor_to_audio(mel_tensor, output_path, model):
     sample_rate = model.h.sample_rate
     sf.write(output_path, wav_gen_float, sample_rate)
 
-def process_directory(directory, bigvgan_model):
-    # load directory
+def process_directory(directory, bigvgan_model_name):
     pt_files = [f for f in os.listdir(directory) if f.endswith('.pt')]
     
     print(f"Found {len(pt_files)} .pt files in the directory.")
     
-    # load model
-    model = bigvgan.BigVGAN.from_pretrained(f'nvidia/{bigvgan_model}', use_cuda_kernel=False)
-    model.remove_weight_norm()
-    model = model.eval().to(device)
-    
-    # process tensors
     for pt_file in tqdm(pt_files, desc="Processing files", unit="file"):
         pt_path = os.path.join(directory, pt_file)
         mel_tensor = torch.load(pt_path)
@@ -39,7 +33,7 @@ def process_directory(directory, bigvgan_model):
         wav_filename = os.path.splitext(pt_file)[0] + ".wav"
         wav_path = os.path.join(directory, wav_filename)
         
-        convert_bigvgan_tensor_to_audio(mel_tensor, wav_path, model)
+        convert_bigvgan_tensor_to_audio(mel_tensor, wav_path, bigvgan_model_name)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Convert .pt files to .wav using BigVGAN.")
@@ -58,4 +52,4 @@ if __name__ == "__main__":
                         help='If the bigvgan method for mel-spectrogram generation is selected, pick which pre-trained model to use as the configuration.')
     args = parser.parse_args()
     
-    process_directory(args.directory, args.bigvgan_model)
+    process_directory(args.directory, args.bigvgan_model_name)
